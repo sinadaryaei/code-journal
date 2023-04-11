413ff01bdd35267db242ffa0e7391a4843962731
sinadaryaei <sinatangssiran@gmail.com>
2023-04-11 17:06:55 +0000

    Add a 'submit' event listener to the form element that:

--- js/main.js
+++ js/main.js
@@ -4,3 +4,25 @@ const photoPreview = document.querySelector('#photo-preview');
 photoUrlInput.addEventListener('input', function () {
   photoPreview.src = photoUrlInput.value;
 });
+const formElement = document.querySelector('#form-element');
+
+formElement.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const newEntry = {
+    photoUrl: formElement.elements['photo-url'].value,
+    title: formElement.elements.title.value,
+    notes: formElement.elements.notes.value
+  };
+
+  newEntry.entryId = data.nextEntryId;
+
+  data.nextEntryId++;
+
+  data.entries.unshift(newEntry);
+
+  const photoPreview = document.querySelector('#photo-preview');
+  photoPreview.src = 'images/placeholder-image-square.jpg';
+
+  formElement.reset();
+});
