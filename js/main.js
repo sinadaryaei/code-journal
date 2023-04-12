c604f62a1d9847d6878fe9794881b73275473393
sinadaryaei <sinatangssiran@gmail.com>
2023-04-12 23:55:11 +0000

    Create an event listener on the document which listens for the DOMContentLoaded event.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// querySelector method of the document object is called with on argument the id user-photo
+// is being assigned to the variable photoUrlInput
+
 const photoUrlInput = document.querySelector('#user-photo');
 const photoPreview = document.querySelector('#photo-preview');
 
@@ -26,3 +29,32 @@ formElement.addEventListener('submit', function (event) {
 
   formElement.reset();
 });
+function renderEntry(entry) {
+  const li = document.createElement('li');
+
+  const div = document.createElement('div');
+  div.className = 'row';
+  li.appendChild(div);
+
+  const columnHalf = document.createElement('div');
+  columnHalf.className = 'column-half';
+  div.appendChild(columnHalf);
+
+  const img = document.createElement('img');
+  img.className = 'entry-image';
+  img.setAttribute('src', entry.photoUrl);
+  columnHalf.appendChild(img);
+
+  const columnHalfText = document.createElement('div');
+  columnHalfText.className = 'column-half';
+  div.appendChild(columnHalfText);
+
+  const h2 = document.createElement('h2');
+  h2.textContent = entry.title;
+  columnHalfText.appendChild(h2);
+
+  const p = document.createElement('p');
+  p.textContent = entry.notes;
+  columnHalfText.appendChild(p);
+  return li;
+}
